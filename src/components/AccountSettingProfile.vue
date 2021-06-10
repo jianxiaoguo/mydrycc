1633cfa70a781e88152a8b489b32d01b994d8db2
lijianguo <lijianguo@zhumengyuan.com>
2021-06-10 11:47:30 +0800

    add account balance

--- src/components/AccountSettingProfile.vue
+++ src/components/AccountSettingProfile.vue
@@ -11,7 +11,7 @@
             <div class="profile edit-first">
                 <div class="ember-view">
                     <div class="ember-view">
-                            <label class="hk-label db mb2" for="new-e-mail">Email Address</label>
+                            <label class="hk-label db mb2">Email Address</label>
                             <div class="flex flex-column flex-row-ns items-center-ns w-100">
                                 <input class="mr2 mb2 w-100 mw6 ember-text-field hk-input--read-only ember-view" type="text" readonly :value="user.email">
                             </div>
@@ -20,11 +20,19 @@
                 </div>
                 <div class="ember-view">
                     <div class="ember-view">
-                            <label class="hk-label db mb2" for="name">Name (Optional)</label>
+                            <label class="hk-label db mb2">Name (Optional)</label>
                             <div class="flex flex-column flex-row-ns items-center-ns w-100">
                                 <input class="mr2 mb2 w-100 mw6 ember-text-field hk-input--read-only ember-view" type="text" readonly :value="user.username">
                             </div>
                     </div>
+                </div>
+                <div class="ember-view">
+                  <div class="ember-view">
+                    <label class="hk-label db mb2">Balance</label>
+                    <div class="flex flex-column flex-row-ns items-center-ns w-100">
+                      <input class="mr2 mb2 w-100 mw6 ember-text-field hk-input--read-only ember-view" type="text" readonly :value="balance">
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
@@ -34,6 +42,7 @@
 <script>
     import {onMounted, reactive, toRefs} from "vue";
     import { getUser, dealUser } from '../services/user'
+    import {getAccountFundingList,dealAccountFundingList} from '../services/funding'
 
     export default {
         name: "AccountSettingProfile",
@@ -41,13 +50,16 @@
             const state = reactive({
                 user :{
                     username: null,
-                    email: null,
-                }
+                    email: null
+                },
+                balance: null,
              })
 
             onMounted(async () => {
-                const res = await getUser()
+                let res = await getUser()
                 state.user = dealUser(res)
+                res = await getAccountFundingList(null, null, 'limit=1')
+                state.balance = res.data ?  dealAccountFundingList(res)[0].balance : 0
             })
             return {
                 ...toRefs(state),
